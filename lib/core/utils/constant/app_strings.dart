fa230a918c05f93765cdd0fc97640ceb0fc68847
walid barakat <walidmohamedbarakat@gmail.com>
2025-10-19 20:31:39 +0300

    fff

--- lib/core/utils/constant/app_strings.dart
+++ lib/core/utils/constant/app_strings.dart
@@ -1,3 +1,10 @@
 class AppStrings {
   static const String appName = "Ma2mouria";
+  static const String yes = "Yes";
+  static const String no = "No";
+  static const String loading = "Loading";
+  static const String warning = "Warning";
+  static const String closeApp = "Close app";
+  static const String error = "Error";
+  static const String ok = "Ok";
 }
